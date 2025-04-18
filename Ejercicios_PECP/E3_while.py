4770659e42541032295701d3cbca99412d136588
jysusdev <jesu.munoz@duocuc.cl>
2025-04-18 00:14:47 -0400

    Correccion ejercicio 3

--- Ejercicios_PECP/E3_while.py
+++ Ejercicios_PECP/E3_while.py
@@ -4,10 +4,13 @@ Su pirámide es un poco rara, ya que en realidad es una pared en forma de pirám
  inferior contiene un bloque más que la capa superior.
 """
 bloques = int(input("Ingrese cantidad de bloques: "))
-acum = bloques
-cont = 0
+bloques_usados = 0
+cont = 1
 
-while acum + cont <= bloques:
-    cont += 1
-    acum = acum - cont
-print (cont)
+while bloques_usados + cont <= bloques:
+    print ("*" * cont)
+    bloques_usados += cont
+    if bloques_usados + cont <= bloques:
+        cont += 1
+print (f"La altura es de {cont} bloques.")
+
